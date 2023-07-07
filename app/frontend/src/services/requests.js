37d95dc7fdd624c0f06143e8f2e69e73620e48b4
cafe51 <cafecafe51@hotmail.com>
2023-07-07 13:52:21 -0400

    fix: conexao com api

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
-const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
+// const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
+// const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
+// const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
+const HOST = 'trybe-futebol-clube-backend.up.railway.app';
+const PROTOCOL = 'https';
 
 const api = axios.create({
   baseURL: `${PROTOCOL}://${HOST}`,
+  // baseURL: 'https://trybe-futebol-clube-backend.up.railway.app/',
 });
 
 export const setToken = (token) => {
